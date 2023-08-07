87cfbafb0df7ba56548d05364348be88775df076
Matuesz Mocydlarz <mateusz.mocydlarz@bitmm.eu>
2023-08-07 12:10:18 +0200

    działająca część core

--- free_simple_media_organizer/model/db_models.py
+++ free_simple_media_organizer/model/db_models.py
@@ -1,53 +1,39 @@
-from sqlalchemy import Table, Column, Integer, String, ForeignKey, MetaData, create_engine
-from typing import List
-from typing import Optional
-from sqlalchemy.orm import Mapped, mapped_column, relationship
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, ForeignKey
 
 tmp_path = r"\\Mocny-nas\hdd_data\010_Mateusz\programming\tmp_data\free-simple-media-organizer\test.db"
 
 engine = create_engine(f"sqlite:///{tmp_path}", echo=True)
-
 metadata_obj = MetaData()
-Base = declarative_base()
 
-user_table = Table(
-    "user_account",
+# ścieżki do katalogów z plikami
+table_directories_paths = Table(
+    "directories_paths",
     metadata_obj,
     Column("id", Integer, primary_key=True),
-    Column("name", String(30)),
-    Column("fullname", String),
+    Column("path", String(300), nullable=False)
 )
 
-address_table = Table(
-    "address",
+# słownik z typami plików
+table_files_types = Table(
+    "files_types",
     metadata_obj,
     Column("id", Integer, primary_key=True),
-    Column("user_id", ForeignKey("user_account.id"), nullable=False),
-    Column("email_address", String, nullable=False),
+    Column("filetype", String(30), nullable=False),
 )
 
+# podstawowe informacja o plikach
+table_files = Table(
+    "files",
+    metadata_obj,
+    Column("id", Integer, primary_key=True),
+    Column("directories_paths_id", ForeignKey("directories_paths.id"), nullable=False),
+    Column("files_types_id", ForeignKey("files_types.id"), nullable=False),
+    Column("filename", String(300), nullable=False),
+    Column("filesize", Integer, nullable=False)
+)
 
-class User(Base):
-    __tablename__ = "user_account"
-    id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str] = mapped_column(String(30))
-    fullname: Mapped[Optional[str]]
-    addresses: Mapped[List["Address"]] = relationship(back_populates="user")
-
-    def __repr__(self) -> str:
-        return f"User(id={self.id!r}, name={self.name!r}, fullname={self.fullname!r})"
-
-
-class Address(Base):
-    __tablename__ = "address"
-    id: Mapped[int] = mapped_column(primary_key=True)
-    email_address: Mapped[str]
-    user_id = mapped_column(ForeignKey("user_account.id"))
-    user: Mapped[User] = relationship(back_populates="addresses")
-
-    def __repr__(self) -> str:
-        return f"Address(id={self.id!r}, email_address={self.email_address!r})"
-
-
+metadata_obj.drop_all(engine)
 metadata_obj.create_all(engine)
+
+# with engine.connect() as conn:
+#     pass
