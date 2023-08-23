c1cfab25c2a1357888e93588355e81bfce21d3e7
Mateusz Mocydlarz <mateusz.mocydlarz@bitmm.eu>
2023-08-23 23:15:41 +0200

    bez entryconfig

--- free_simple_media_organizer/mainwindow.py
+++ free_simple_media_organizer/mainwindow.py
@@ -19,35 +19,38 @@ class MainWindow(tk.Tk):
         self.menu_bar.add_cascade(label=_("Database"), menu=self.menu_db)
         self.menu_db.add_command(label=_("New..."), command=self.dialog_new_db)
         self.menu_db.add_command(label=_("Open..."))
-        self.menu_db.add_command(label=_("Close database"))
+        self.menu_db.add_command(label=_("Close database"), state=tk.DISABLED)
         self.menu_db.add_separator()
-        self.menu_db.add_command(label=_("Settings..."), command=self.dialog_db_settings)
-        self.menu_db.entryconfig(_("Close database"), state=tk.DISABLED)
-        self.menu_db.entryconfig(_("Settings..."), state=tk.DISABLED)
+        self.menu_db.add_command(label=_("Settings..."), command=self.dialog_db_settings, state=tk.DISABLED)
 
         self.menu_media = tk.Menu(self.menu_bar, tearoff=0)
         self.menu_bar.add_cascade(label=_("Media"), menu=self.menu_media)
-        self.menu_media.add_command(label=_("Sources..."))
+        self.menu_media.add_command(label=_("Sources..."), state=tk.DISABLED)
 
         self.menu_settings = tk.Menu(self.menu_bar, tearoff=0)
         self.menu_bar.add_cascade(label=_("Settings"), menu=self.menu_settings)
 
         self.config(menu=self.menu_bar)
 
-    def open_dialog(self):
-        self.menu_bar.entryconfig(_("Database"), state=tk.DISABLED)
-        self.menu_bar.entryconfig(_("Media"), state=tk.DISABLED)
-        self.menu_bar.entryconfig(_("Settings"), state=tk.DISABLED)
+    def menu_bar_state(self, in_state: bool):
+        if in_state is False:
+            tk_state = tk.DISABLED
+        else:
+            tk_state = tk.NORMAL
+
+        self.menu_bar.entryconfig(_("Database"), state=tk_state)
+        self.menu_bar.entryconfig(_("Media"), state=tk_state)
+        self.menu_bar.entryconfig(_("Settings"), state=tk_state)
 
     def dialog_new_db(self):
-        self.open_dialog()
+        self.menu_bar_state(False)
         dialog_new_db = dialognewdb.DialogNewDb(self)
-        dialog_new_db.grab_set_global()
+        dialog_new_db.grab_set()
 
     def dialog_db_settings(self):
-        self.open_dialog()
+        self.menu_bar_state(False)
         dialog_new_db = dialogdbsettings.DialogDbSettings(self)
-        dialog_new_db.grab_set_global()
+        dialog_new_db.grab_set()
 
 
 if __name__ == "__main__":
