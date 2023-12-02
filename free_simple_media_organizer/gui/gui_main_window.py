0669caba049671dbf10e4bfcd1085b1ccdffeb28
Matuesz Mocydlarz <mateusz.mocydlarz@bitmm.eu>
2023-12-02 23:07:03 +0100

    main window

--- free_simple_media_organizer/gui/gui_main_window.py
+++ free_simple_media_organizer/gui/gui_main_window.py
@@ -16,16 +16,16 @@ class GuiMainWindow(tk.Tk):
         self.menu_bar = tk.Menu(self)
 
         self.menu_db = tk.Menu(self.menu_bar, tearoff=0)
-        self.menu_db.add_command(label="New...")
+        self.menu_db.add_command(label="New...", command=self.new_db)
         self.menu_db.add_command(label="Open..")
 
         self.menu_open_last = tk.Menu(self.menu_db, tearoff=0)
         self.menu_db.add_cascade(label="Open last", menu=self.menu_open_last)
         self.menu_db.add_separator()
-        self.menu_db.add_command(label="Close")
+        self.menu_db.add_command(label="Close", command=self.app_close)
 
         self.menu_settings = tk.Menu(self.menu_bar, tearoff=0)
-        self.menu_settings.add_command(label="Aplication...")
+        self.menu_settings.add_command(label="Aplication...", command=self.app_settings)
         self.menu_settings.add_command(label="Database...")
 
         self.menu_help = tk.Menu(self.menu_bar, tearoff=0)
@@ -39,6 +39,15 @@ class GuiMainWindow(tk.Tk):
 
     #     # self.menu_open_last.add_command(label="1")
     #     # self.menu_open_last.add_command(label="2")
+
+    def new_db(self):
+        print("New db")
+
+    def app_close(self):
+        self.destroy()
+
+    def app_settings(self):
+        print("Settings")
 
 
 if __name__ == "__main__":
